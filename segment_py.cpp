54a5a7985415c93ea34d7598cff02289e58426ac
Daichi SUZUO <belltailjp@gmail.com>
2015-04-23 01:07:08 +0900

    Wrote error handling

--- segment_py.cpp
+++ segment_py.cpp
@@ -3,9 +3,34 @@
 #include <boost/numpy.hpp>
 #include "segment/segment-image.h"
 
-boost::numpy::ndarray segment(const boost::numpy::ndarray& img, float sigma, float c, int min_size)
+boost::numpy::ndarray segment(const boost::numpy::ndarray& input_image, float sigma, float c, int min_size)
 {
-    return img;
+    const int nd = input_image.get_nd();
+    if(nd != 3)
+    {
+        throw std::runtime_error("input_image must be 3-dimensional");
+    }
+
+    const int h = input_image.shape(0);
+    const int w = input_image.shape(1);
+    const int depth = input_image.shape(2);
+
+    if(depth != 3)
+    {
+        throw std::runtime_error("input_image must have rgb channel");
+    }
+
+    if(input_image.get_dtype() != boost::numpy::dtype::get_builtin<unsigned char>())
+    {
+        throw std::runtime_error("dtype of input_image must be uint8");
+    }
+
+    if(!input_image.get_flags() & boost::numpy::ndarray::C_CONTIGUOUS)
+    {
+        throw std::runtime_error("input_image must be C-style contiguous");
+    }
+
+    return input_image;
 }
 
 BOOST_PYTHON_MODULE(segment)
