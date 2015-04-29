cf2fad2e07a0d713ac3b35866cf451c00fdde53c
Daichi SUZUO <belltailjp@gmail.com>
2015-04-29 21:28:57 +0900

    Refactored color histogram extractor by utilizing numpy histogram2d

--- selective_search_features.py
+++ selective_search_features.py
@@ -8,14 +8,12 @@ def color_histgram(input_img, label_img, n_region):
     n_bin = 25
     bin_width = int(math.ceil(255.0 / n_bin))
 
-    h, w = label_img.shape
-    hist = numpy.zeros((n_region, 25 * 3))
-    for y in range(h):
-        for x in range(w):
-            label = label_img[y, x]
-            r, g, b = input_img[y, x] / bin_width
-            hist[label, int(r) + 50] += 1
-            hist[label, int(g) + 25] += 1
-            hist[label, int(b)     ] += 1
-    return hist
+    bins_color = [i * bin_width for i in range(n_bin + 1)]
+    bins_label = range(n_region + 1)
+    bins = [bins_label, bins_color]
+
+    r_hist = numpy.histogram2d(label_img.ravel(), input_img[:, :, 0].ravel(), bins=bins)[0] #shape=(n_region, n_bin)
+    g_hist = numpy.histogram2d(label_img.ravel(), input_img[:, :, 1].ravel(), bins=bins)[0]
+    b_hist = numpy.histogram2d(label_img.ravel(), input_img[:, :, 2].ravel(), bins=bins)[0]
+    return numpy.hstack([r_hist, g_hist, b_hist])
 
