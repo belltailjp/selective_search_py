98ca04444ce77a8bf9858bd81a82594f24846424
Daichi SUZUO <belltailjp@gmail.com>
2015-05-05 02:00:59 +0900

    Implemented color similarity

--- similarity.py
+++ similarity.py
@@ -6,3 +6,9 @@ import numpy
 def size(size1, size2, size_img):
     return 1. - float(size1 + size2) / size_img
 
+def __histogram_intersection(vec1, vec2):
+    return numpy.sum(numpy.minimum(vec1, vec2), axis = 1)
+
+def color(hist1, hist2):
+    return __histogram_intersection(hist1, hist2)
+
--- test_similarity.py
+++ test_similarity.py
@@ -13,3 +13,19 @@ class TestSimilaritySize:
         ret = similarity.size(10, 20, 100)
         assert ret == 0.7
 
+class TestSimilarityColor:
+    def test_1region(self):
+        ar1 = numpy.array([[1] * 75])
+        ar2 = numpy.array([[2] * 75])
+        t = similarity.color(ar1, ar2)
+        assert t.shape == (1, )
+        assert numpy.array_equal(t, [75])
+
+    def test_multiregion(self):
+        # build (5, 75) array whose i-th row is filled with i (or 2i)
+        ar1 = numpy.array([[i * 2] * 75 for i in range(5)])
+        ar2 = numpy.array([[i]     * 75 for i in range(5)])
+        t = similarity.color(ar1, ar2)
+        assert t.shape == (5, )
+        assert numpy.array_equal(t, [0, 75, 150, 225, 300])
+
