8770eab3c9bf3bdc6eee5c158d22cc2457684b8e
Daichi SUZUO <belltailjp@gmail.com>
2015-05-05 01:22:27 +0900

    Changed module name

--- test_features.py
+++ test_features.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import numpy
-import selective_search_features
+import features
 
-class TestSelectiveSearchFeaturesColorHistogram:
+class TestFeaturesColorHistogram:
     def setup_method(self, method = None, w = 10, h = 10):
         self.h, self.w = h, w
         self.input_img = numpy.zeros((self.h, self.w, 3), dtype=int)
         self.label_img = numpy.zeros((self.h, self.w), dtype=int)
 
     def test_1region_1color(self):
-        hist = selective_search_features.color_histgram(self.input_img, self.label_img, 1)
+        hist = features.color_histgram(self.input_img, self.label_img, 1)
         assert hist.shape == (1, 75)
         r_expected = [0.333333333] + [0] * 24
         g_expected = [0.333333333] + [0] * 24
@@ -23,7 +23,7 @@ class TestSelectiveSearchFeaturesColorHistogram:
         for y in range(self.h):
             self.input_img[y, :, :] = y
 
-        hist = selective_search_features.color_histgram(self.input_img, self.label_img, 1)
+        hist = features.color_histgram(self.input_img, self.label_img, 1)
         assert hist.shape == (1, 75)
         r_expected = [11] * 23 + [3, 0] # because bin width equals 11
         g_expected = [11] * 23 + [3, 0]
@@ -36,36 +36,36 @@ class TestSelectiveSearchFeaturesColorHistogram:
         for y in range(self.h):
             self.label_img[y, :] = y
 
-        hist = selective_search_features.color_histgram(self.input_img, self.label_img, 2)
+        hist = features.color_histgram(self.input_img, self.label_img, 2)
         assert hist.shape == (2, 75)
         r1_expected = ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24)
         r2_expected = ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24)
         numpy.testing.assert_array_almost_equal(hist[0].ravel(), r1_expected)
         numpy.testing.assert_array_almost_equal(hist[1].ravel(), r2_expected)
 
-class TestSelectiveSearchFeaturesSize:
+class TestFeaturesSize:
     def setup_method(self, method):
         self.label_img = numpy.zeros((10, 10), dtype=int)
 
     def test_1region(self):
-        sizes = selective_search_features.size(self.label_img, 2)
+        sizes = features.size(self.label_img, 2)
         assert sizes.shape == (2,)
         assert sizes[0] == 100
         assert sizes[1] == 0
 
     def test_2region(self):
         self.label_img[:5, :] = 1
-        sizes = selective_search_features.size(self.label_img, 2)
+        sizes = features.size(self.label_img, 2)
         assert sizes.shape == (2,)
         assert sizes[0] == 50
         assert sizes[1] == 50
 
-class TestSelectiveSearchFeaturesBoundingBox:
+class TestFeaturesBoundingBox:
     def setup_method(self, method):
         self.label_img = numpy.zeros((10, 10), dtype=int)
 
     def test_1region(self):
-        bb = selective_search_features.bounding_box(self.label_img, 1)
+        bb = features.bounding_box(self.label_img, 1)
         assert bb.shape == (1, 4)
         assert numpy.array_equal(bb[0], [0, 0, 9, 9])
 
@@ -74,7 +74,7 @@ class TestSelectiveSearchFeaturesBoundingBox:
         self.label_img[:5, 5:] = 1
         self.label_img[5:, :5] = 2
         self.label_img[5:, 5:] = 3
-        bb = selective_search_features.bounding_box(self.label_img, 4)
+        bb = features.bounding_box(self.label_img, 4)
         assert bb.shape == (4, 4)
         assert numpy.array_equal(bb[0], [0, 0, 4, 4])
         assert numpy.array_equal(bb[1], [0, 5, 4, 9])
