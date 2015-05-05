a262b8bcf19f6beec1f3489e2ce54fb8e3ce3247
Daichi SUZUO <belltailjp@gmail.com>
2015-05-05 17:09:55 +0900

    Refactored adjacency matrix

--- selective_search.py
+++ selective_search.py
@@ -7,16 +7,14 @@ import collections
 
 def calc_adjacency_matrix(label_img, n_region):
     A = numpy.diag([1] * n_region)
-    h, w = label_img.shape[0], label_img.shape[1]
-    for y in range(h):
-        for x in range(w):
-            here = label_img[y, x]
-            if y < h - 1:
-                b = label_img[y + 1, x]
-                A[here, b] = A[b, here] = 1
-            if x < w - 1:
-                r = label_img[y, x + 1]
-                A[here, r] = A[r, here] = 1
+    h, w = label_img.shape
+    for ((y, x), here) in numpy.ndenumerate(label_img):
+        if y < h - 1:
+            b = label_img[y + 1, x]
+            A[here, b] = A[b, here] = 1
+        if x < w - 1:
+            r = label_img[y, x + 1]
+            A[here, r] = A[r, here] = 1
 
     A = numpy.triu(A)
     dic = {i : {j for (j, Aij) in enumerate(Ai) if Aij == 1 and i != j} for (i, Ai) in enumerate(A)}
