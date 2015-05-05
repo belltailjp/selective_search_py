4baca1930c0aa7251a162e7d05159f80302574ca
Daichi SUZUO <belltailjp@gmail.com>
2015-05-05 14:31:16 +0900

    Unified initial feature caluculators into a class

--- features.py
+++ features.py
@@ -7,64 +7,79 @@ import skimage
 import skimage.filters
 import scipy.ndimage.filters
 
-def color_histgram(input_img, label_img, n_region):
-    n_bin = 25
-    bin_width = int(math.ceil(255.0 / n_bin))
+class Features:
+    def __init__(self, image, label, n_region, similarity_weight):
+        self.image = image
+        self.label = label
+        self.w     = similarity_weight
 
-    bins_color = [i * bin_width for i in range(n_bin + 1)]
-    bins_label = range(n_region + 1)
-    bins = [bins_label, bins_color]
+        self.imsize  = float(label.shape[0] * label.shape[1])
+        self.size    = self.__init_size(n_region)
+        self.color   = self.__init_color(n_region)
+        self.bbox    = self.__init_bounding_box(n_region)
+        self.texture = self.__init_texture(n_region)
 
-    r_hist = numpy.histogram2d(label_img.ravel(), input_img[:, :, 0].ravel(), bins=bins)[0] #shape=(n_region, n_bin)
-    g_hist = numpy.histogram2d(label_img.ravel(), input_img[:, :, 1].ravel(), bins=bins)[0]
-    b_hist = numpy.histogram2d(label_img.ravel(), input_img[:, :, 2].ravel(), bins=bins)[0]
-    hist = numpy.hstack([r_hist, g_hist, b_hist])
-    l1_norm = numpy.sum(hist, axis = 1).reshape((n_region, 1))
+    def __init_size(self, n_region):
+        bincnt = numpy.bincount(self.label.ravel(), minlength = n_region)
+        return {i : bincnt[i] for i in range(n_region)}
 
-    return numpy.nan_to_num(hist / l1_norm)
+    def __init_color(self, n_region):
+        n_bin = 25
+        bin_width = int(math.ceil(255.0 / n_bin))
 
+        bins_color = [i * bin_width for i in range(n_bin + 1)]
+        bins_label = range(n_region + 1)
+        bins = [bins_label, bins_color]
 
-def size(label_img, n_region):
-    return numpy.bincount(label_img.ravel(), minlength = n_region)
+        r_hist = numpy.histogram2d(self.label.ravel(), self.image[:, :, 0].ravel(), bins=bins)[0] #shape=(n_region, n_bin)
+        g_hist = numpy.histogram2d(self.label.ravel(), self.image[:, :, 1].ravel(), bins=bins)[0]
+        b_hist = numpy.histogram2d(self.label.ravel(), self.image[:, :, 2].ravel(), bins=bins)[0]
+        hist = numpy.hstack([r_hist, g_hist, b_hist])
+        l1_norm = numpy.sum(hist, axis = 1).reshape((n_region, 1))
 
+        hist = numpy.nan_to_num(hist / l1_norm)
+        return {i : hist[i] for i in range(n_region)}
 
-def bounding_box(label_img, n_region):
-    B = numpy.full((n_region, 4), fill_value = float('NaN'))
-    for ((i, j), label) in numpy.ndenumerate(label_img):
-        (i1, j1, i2, j2) = B[label]
-        B[label] = min(i, i1), min(j, j1), max(i, i2), max(j, j2)
+    def __init_bounding_box(self, n_region):
+        B = numpy.full((n_region, 4), fill_value = float('NaN'))
+        for ((i, j), idx) in numpy.ndenumerate(self.label):
+            (i1, j1, i2, j2) = B[idx]
+            B[idx] = min(i, i1), min(j, j1), max(i, i2), max(j, j2)
+        return {i : B[i] for i in range(n_region)}
 
-    return B
+    def __init_texture(self, n_region):
+        ar = numpy.ndarray((n_region, 240))
+        return {i : ar[i] for i in range(n_region)}
 
+    def __calc_gradient_histogram(self, label, gaussian, n_region, nbins_orientation = 8, nbins_inten = 10):
+        op = numpy.array([[-1, 0, 1]], dtype=numpy.float32)
+        h = scipy.ndimage.filters.convolve(gaussian, op)
+        v = scipy.ndimage.filters.convolve(gaussian, op.transpose())
+        g = numpy.arctan2(v, h)
 
-def __gradient_histogram(label_img, gaussian, n_region, nbins_orientation = 8, nbins_inten = 10):
-    op = numpy.array([[-1, 0, 1]], dtype=numpy.float32)
-    h = scipy.ndimage.filters.convolve(gaussian, op)
-    v = scipy.ndimage.filters.convolve(gaussian, op.transpose())
-    g = numpy.arctan2(v, h)
+        # define each axis for texture histogram
+        bin_width = 2 * math.pi / 8
+        bins_label = range(n_region + 1)
+        bins_angle = numpy.linspace(-math.pi, math.pi, nbins_orientation + 1)
+        bins_inten = numpy.linspace(.0, 1., nbins_inten + 1)
+        bins = [bins_label, bins_angle, bins_inten]
 
-    # define each axis for texture histogram
-    bin_width = 2 * math.pi / 8
-    bins_label = range(n_region + 1)
-    bins_angle = numpy.linspace(-math.pi, math.pi, nbins_orientation + 1)
-    bins_inten = numpy.linspace(.0, 1., nbins_inten + 1)
-    bins = [bins_label, bins_angle, bins_inten]
+        # calculate 3 dimensional histogram
+        ar = numpy.vstack([label.ravel(), g.ravel(), gaussian.ravel()]).transpose()
+        hist = numpy.histogramdd(ar, bins = bins)[0]
 
-    # calculate 3 dimensional histogram
-    ar = numpy.vstack([label_img.ravel(), g.ravel(), gaussian.ravel()]).transpose()
-    hist = numpy.histogramdd(ar, bins = bins)[0]
+        # orientation_wise intensity histograms are serialized for each region
+        return numpy.reshape(hist, (n_region, nbins_orientation * nbins_inten))
 
-    # orientation_wise intensity histograms are serialized for each region
-    return numpy.reshape(hist, (n_region, nbins_orientation * nbins_inten))
+    def __init_texture(self, n_region):
+        gaussian = skimage.filters.gaussian_filter(self.image, sigma = 1.0, multichannel = True).astype(numpy.float32)
+        r_hist = self.__calc_gradient_histogram(self.label, gaussian[:, :, 0], n_region)
+        g_hist = self.__calc_gradient_histogram(self.label, gaussian[:, :, 1], n_region)
+        b_hist = self.__calc_gradient_histogram(self.label, gaussian[:, :, 2], n_region)
 
-def texture(input_img, label_img, n_region):
-    gaussian = skimage.filters.gaussian_filter(input_img, sigma = 1.0, multichannel = True).astype(numpy.float32)
-    r_hist = __gradient_histogram(label_img, gaussian[:, :, 0], n_region)
-    g_hist = __gradient_histogram(label_img, gaussian[:, :, 1], n_region)
-    b_hist = __gradient_histogram(label_img, gaussian[:, :, 2], n_region)
+        hist = numpy.hstack([r_hist, g_hist, b_hist])
+        l1_norm = numpy.sum(hist, axis = 1).reshape((n_region, 1))
 
-    hist = numpy.hstack([r_hist, g_hist, b_hist])
-    l1_norm = numpy.sum(hist, axis = 1).reshape((n_region, 1))
-
-    return numpy.nan_to_num(hist / l1_norm)
+        hist = numpy.nan_to_num(hist / l1_norm)
+        return {i : hist[i] for i in range(n_region)}
 
--- test_features.py
+++ test_features.py
@@ -7,75 +7,84 @@ import features
 class TestFeaturesColorHistogram:
     def setup_method(self, method = None, w = 10, h = 10):
         self.h, self.w = h, w
-        self.input_img = numpy.zeros((self.h, self.w, 3), dtype=int)
-        self.label_img = numpy.zeros((self.h, self.w), dtype=int)
+        image = numpy.zeros((self.h, self.w, 3), dtype=numpy.uint8)
+        label = numpy.zeros((self.h, self.w), dtype=int)
+        self.f = features.Features(image, label, 1, None)
 
     def test_1region_1color(self):
-        hist = features.color_histgram(self.input_img, self.label_img, 1)
-        assert hist.shape == (1, 75)
+        hist = self.f._Features__init_color(1)
+        assert len(hist) == 1
+        assert hist[0].shape == (75,)
         r_expected = [0.333333333] + [0] * 24
         g_expected = [0.333333333] + [0] * 24
         b_expected = [0.333333333] + [0] * 24
-        numpy.testing.assert_array_almost_equal(hist.ravel(), r_expected + g_expected + b_expected)
+        numpy.testing.assert_array_almost_equal(hist[0].ravel(), r_expected + g_expected + b_expected)
 
     def test_1region_255color(self):
         self.setup_method(self, w = 1, h = 256)
         for y in range(self.h):
-            self.input_img[y, :, :] = y
+            self.f.image[y, :, :] = y
 
-        hist = features.color_histgram(self.input_img, self.label_img, 1)
-        assert hist.shape == (1, 75)
+        hist = self.f._Features__init_color(1)
+        assert len(hist) == 1
+        assert hist[0].shape == (75,)
         r_expected = [11] * 23 + [3, 0] # because bin width equals 11
         g_expected = [11] * 23 + [3, 0]
         b_expected = [11] * 23 + [3, 0]
         expected = numpy.array(r_expected + g_expected + b_expected)
-        numpy.testing.assert_array_almost_equal(hist.ravel(), expected / numpy.sum(expected))
+        numpy.testing.assert_array_almost_equal(hist[0].ravel(), expected / numpy.sum(expected))
 
     def test_2region_1color(self):
         self.setup_method(self, w = 1, h = 2)
         for y in range(self.h):
-            self.label_img[y, :] = y
+            self.f.label[y, :] = y
 
-        hist = features.color_histgram(self.input_img, self.label_img, 2)
-        assert hist.shape == (2, 75)
+        hist = self.f._Features__init_color(2)
+        assert len(hist) == 2
+        assert hist[0].shape == (75,)
         r1_expected = ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24)
         r2_expected = ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24) + ([1.0/3] + [0] * 24)
         numpy.testing.assert_array_almost_equal(hist[0].ravel(), r1_expected)
         numpy.testing.assert_array_almost_equal(hist[1].ravel(), r2_expected)
 
+
 class TestFeaturesSize:
     def setup_method(self, method):
-        self.label_img = numpy.zeros((10, 10), dtype=int)
+        image = numpy.zeros((10, 10, 3), dtype=numpy.uint8)
+        label = numpy.zeros((10, 10), dtype=int)
+        self.f = features.Features(image, label, 1, None)
 
     def test_1region(self):
-        sizes = features.size(self.label_img, 2)
-        assert sizes.shape == (2,)
+        sizes = self.f._Features__init_size(1)
+        assert len(sizes) == 1
         assert sizes[0] == 100
-        assert sizes[1] == 0
 
     def test_2region(self):
-        self.label_img[:5, :] = 1
-        sizes = features.size(self.label_img, 2)
-        assert sizes.shape == (2,)
+        self.f.label[:5, :] = 1
+        sizes = self.f._Features__init_size(2)
+        assert len(sizes) == 2
         assert sizes[0] == 50
         assert sizes[1] == 50
 
 class TestFeaturesBoundingBox:
     def setup_method(self, method):
-        self.label_img = numpy.zeros((10, 10), dtype=int)
+        image = numpy.zeros((10, 10, 3), dtype=numpy.uint8)
+        label = numpy.zeros((10, 10), dtype=int)
+        self.f = features.Features(image, label, 1, None)
 
     def test_1region(self):
-        bb = features.bounding_box(self.label_img, 1)
-        assert bb.shape == (1, 4)
+        bb = self.f._Features__init_bounding_box(1)
+        assert len(bb) == 1
+        assert bb[0].shape == (4,)
         assert numpy.array_equal(bb[0], [0, 0, 9, 9])
 
     def test_4region(self):
-        self.label_img[:5, :5] = 0
-        self.label_img[:5, 5:] = 1
-        self.label_img[5:, :5] = 2
-        self.label_img[5:, 5:] = 3
-        bb = features.bounding_box(self.label_img, 4)
-        assert bb.shape == (4, 4)
+        self.f.label[:5, :5] = 0
+        self.f.label[:5, 5:] = 1
+        self.f.label[5:, :5] = 2
+        self.f.label[5:, 5:] = 3
+        bb = self.f._Features__init_bounding_box(4)
+        assert len(bb) == 4
         assert numpy.array_equal(bb[0], [0, 0, 4, 4])
         assert numpy.array_equal(bb[1], [0, 5, 4, 9])
         assert numpy.array_equal(bb[2], [5, 0, 9, 4])
