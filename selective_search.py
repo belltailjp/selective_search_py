dfed4411c0675cbd44f70bf4861fa3cbe9cbe31f
Daichi SUZUO <belltailjp@gmail.com>
2015-05-05 20:17:58 +0900

    Speedup adjacency matrix calculation

--- selective_search.py
+++ selective_search.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import numpy
+import scipy.sparse
 import segment
 import collections
 
 def calc_adjacency_matrix(label_img, n_region):
-    A = numpy.diag([1] * n_region)
-    h, w = label_img.shape
-    for ((y, x), here) in numpy.ndenumerate(label_img):
-        if y < h - 1:
-            b = label_img[y + 1, x]
-            A[here, b] = A[b, here] = 1
-        if x < w - 1:
-            r = label_img[y, x + 1]
-            A[here, r] = A[r, here] = 1
+    r = numpy.vstack([label_img[:, :-1].ravel(), label_img[:, 1:].ravel()])
+    b = numpy.vstack([label_img[:-1, :].ravel(), label_img[1:, :].ravel()])
+    t = numpy.hstack([r, b, r[::-1], b[::-1]])
+    A = scipy.sparse.coo_matrix((numpy.ones(t.shape[1]), (t[0], t[1]))).todense()
+    A = numpy.minimum(A, numpy.ones((n_region, n_region))).astype(int)
+
+    for i in range(n_region):
+        A[i, i] = 1
 
     A = numpy.triu(A)
     dic = {i : {j for (j, Aij) in enumerate(Ai) if Aij == 1 and i != j} for (i, Ai) in enumerate(A)}
