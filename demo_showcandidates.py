6f7ca0d6a068ff82063b77227128013bb2de0961
Daichi SUZUO <belltailjp@gmail.com>
2015-05-17 22:50:58 +0900

    Demo to display what regions are generated

--- demo_showcandidates.py
+++ demo_showcandidates.py
@@ -8,17 +8,58 @@ import numpy
 import skimage.io
 import selective_search
 
-import matplotlib.pyplot
-import PIL.Image
-import PIL.ImageDraw
+from PySide.QtCore import *
+from PySide.QtGui import *
 
-def draw_rects(img, regions):
-    pil_img = PIL.Image.fromarray(img)
-    draw = PIL.ImageDraw.Draw(pil_img)
-    for (value, (y0, x0, y1, x1)) in regions:
-        draw.rectangle([x0, y0, x1, y1], outline = (0, 255, 0))
+class Demo(QWidget):
+    def __init__(self, ndimg, regions):
+        super().__init__()
+        h, w = ndimg.shape[:2]
+        self.qimg = QImage(ndimg.flatten(), w, h, QImage.Format_RGB888)
+        self.regions = regions
 
-    return numpy.asarray(pil_img)
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+        self.__init_imagearea()
+        self.__init_slider()
+
+    def __init_imagearea(self):
+        self.label = QLabel()
+        self.layout.addWidget(self.label, 0, 0)
+
+    def __init_slider(self):
+        hbox = QHBoxLayout()
+
+        label = QLabel()
+        label.setText('count:')
+        hbox.addWidget(label)
+
+        slider = QSlider(Qt.Horizontal)
+        slider.setMinimum(0)
+        slider.setMaximum(len(regions))
+        slider.valueChanged.connect(self.count_changed)
+        slider.setValue(int(len(regions) / 4))
+        hbox.addWidget(slider)
+
+        self.count_label = QLabel()
+        hbox.addWidget(self.count_label)
+
+        self.layout.addLayout(hbox, 1, 0)
+
+
+    def count_changed(self, value):
+        self.__draw(value)
+        self.count_label.setText(str(value))
+
+    def __draw(self, count):
+        self.pixmap = QPixmap(self.qimg)
+        painter = QPainter(self.pixmap)
+        painter.setPen(QColor(0, 255, 0))
+        for v, (y0, x0, y1, x1) in self.regions[:count]:
+            painter.drawRect(x0, y0, x1, y1)
+
+        self.label.setPixmap(self.pixmap)
+
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -30,8 +71,8 @@ if __name__=="__main__":
         img = skimage.color.gray2rgb(img)
 
     regions = selective_search.selective_search(img)
-    result = draw_rects(img, regions)
-
-    skimage.io.imshow(result)
-    skimage.io.show()
+    app = QApplication(sys.argv)
+    wnd = Demo(img, regions)
+    wnd.show()
+    app.exec_()
 
